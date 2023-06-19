e43449ddd5babc7268f27555973d2a6a33172066
Valdemar Burbo <valdemar.burbo@gmail.com>
2023-06-19 15:27:53 +0100

    Link sheets and add objects

---
+++ app.js
@@ -0,0 +1,32 @@
+"use strict"
+
+let shopSeattle = {
+    minHourlyCustomers: 23,
+    maxHourlyCustomers: 63,
+    avgCookiesPerCustomer: 6.3
+};
+
+let shopTokyo = {
+    minHourlyCustomers: 3,
+    maxHourlyCustomers: 24,
+    avgCookiesPerCustomer: 1.2,
+};
+
+let shopDubai = {
+    minHourlyCustomers: 11,
+    maxHourlyCustomers: 38,
+    avgCookiesPerCustomer: 3.7,
+};
+
+let shopParis = {
+    minHourlyCustomers: 20,
+    maxHourlyCustomers: 38,
+    avgCookiesPerCustomer: 2.3,
+};
+
+let shopLima = {
+    minHourlyCustomers: 2,
+    maxHourlyCustomers: 16,
+    avgCookiesPerCustomer: 4.6,
+};
+
